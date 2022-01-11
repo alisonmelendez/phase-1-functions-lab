379a1362bcaa197b2c21ced1f5d6cb1b3a3eee1a
Alison Melendez <alisonmelendez132@gmail.com>
2022-01-10 23:13:29 -0500

    commit tree b8c210bde333e55feb2103f2f6596e9e918789d8

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLoc) {
+    const headquarters = 42;
+    let distance;
+    if (pickupLoc > 42) {
+        distance = pickupLoc - headquarters;
+    } else {
+        distance = headquarters - pickupLoc;
+    }
+    return distance;
+}
+
+//distanceFromHqInBlocks(pickupLoc);
+
+function distanceFromHqInFeet(blocks) {
+    const numOfBlocks = distanceFromHqInBlocks(blocks);
+
+    const convertToFeet = numOfBlocks * 264;
+
+    return convertToFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    let solution = (destination - start) * 264
+
+    if (destination < start) {
+        solution = (solution) * -1
+    }
+    return solution;
+
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    let fee;
+    if (distance <= 400) {
+        fee = 0;
+    } else if (distance > 400 && distance < 2000) {
+        fee = (distance - 400) * .02
+    } else if (distance > 2000 && distance < 2500) {
+        fee = 25
+    } else {
+        fee = 'cannot travel that far'
+    }
+    return fee
+}
+
+calculatesFarePrice(50, 60)
